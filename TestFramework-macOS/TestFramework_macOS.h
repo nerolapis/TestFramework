60ae7219c5a5473273854fe328ad07feabe70669
Konstantin Erokhin <konstantin.erokhin@shinyfrog.net>
2024-03-07 17:25:08 +0100

    Making macOS and visionOS targets shared

--- TestFramework-macOS/TestFramework_macOS.h
+++ TestFramework-macOS/TestFramework_macOS.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT const unsigned char TestFramework_macOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TestFramework_macOS/PublicHeader.h>
 
-
+#import <TestFramework/TestClass.swift>
--- TestFramework-visionOS/TestFramework_visionOS.h
+++ TestFramework-visionOS/TestFramework_visionOS.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT const unsigned char TestFramework_visionOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TestFramework_visionOS/PublicHeader.h>
 
-
+#import <TestFramework/TestClass.swift>
